3531c273228e3dbe8558e86ba8d48d3f4d1a9020
Sudila Jayasekara <mpsjayasekara@gmail.com>
2023-05-16 17:43:26 +0530

    Create login form

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Login Page</title>
+</head>
+<body>
+	<div class="login">
+		<form>
+			<fieldset>
+				<p> <input type="text" name="email" placeholder="Email Address"> </p>
+				<p> <input type="text" name="password" placeholder="Password"> </p>
+				<p> <button type="submit" name="login">Login</button> </p>
+			</fieldset>
+		</form>
+	</div> <!-- login -->
+</body>
+</html>
